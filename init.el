559ba521a6f747baaf3ef1c2210f3c9a1d0c37d8
ingtshan <rongcanyo@gmail.com>
2021-10-22 21:37:21 +0800

    update init for detail

--- init.el
+++ init.el
@@ -1,8 +1,8 @@
-(require 'init-const)                ;;
+(require 'init-const)             ;; control and info
 (require 'init-bas)               ;; basic setting
 (require 'init-pkg)               ;; packages manage tool (use leaf)
-(require 'init-os)                ;; OS 适配：系统快捷键
+(require 'init-os)                ;; OS adapt
 (require 'init-wf)                ;; workflow
-(require 'init-ui)                ;;
+(require 'init-ui)                ;; pretty face and smart interactive
 (require 'init-ns)                ;; build my noting sys
-(require 'init-dev)               ;;
+(require 'init-dev)               ;; dev support
