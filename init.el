d190de7e61a3f83bac0cd9a66a3b55578fa73eba
ingtshan <rongcanyo@gmail.com>
2021-10-23 17:40:23 +0800

    fix dumped init gui issues

--- init.el
+++ init.el
@@ -4,10 +4,9 @@
 (defvar *do-dump* nil
   "Not nil when do-dump.")
 
-;; 拒绝 dump-init
+;; 拒绝 dump-init but do-dump
 (unless *dumped-init-path*
-  ;; normal-init
-  ;; but do-dump
+  ;; normal-init and add to dump
   
   (require 'init-pkg)               ; packages manage tool (use leaf)
   (require 'init-const)             ; control and info
@@ -20,7 +19,7 @@
 
 ;; 拒绝 normal-init
 (when *dumped-init-path*
-  ;; dumped-init
+  ;; dumped-init what is above
   (setq load-path *dumped-init-path*)
   (setq warning-minimum-level :emergency)
   (global-font-lock-mode t)
@@ -28,10 +27,18 @@
 
 ;; load after
 
-;; 拒绝 do-dump
+;; 拒绝 do-dump but init whatever
 (unless *do-dump*
-  ;; init remaining
-  ;; but don't do-dump
+  ;; init remaining add not add to dump
+
+  ;; gui/console
+  ;; put here becaus in dumper can't get right info
+  (defconst *is-app* (and (display-graphic-p) (not (daemonp))))
+  (defconst *is-server-m* (string-equal "main" (daemonp)))
+  (defconst *is-server-c* (string-equal "coding" (daemonp)))
+  (defconst *is-server-t* (string-equal "tty" (daemonp)))
+  (defconst *is-gui*  (or *is-app* *is-server-m* *is-server-c*))
+  (defconst *is-cli* (or (not *is-gui*) *is-server-t*))
   
   (when *is-gui*
     (require 'init-ui)                 ; pretty face and smart interactive
